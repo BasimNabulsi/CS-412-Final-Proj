03083edcad8e12bddf0412dcc3df61e0e05dfb49
Pedro Pardo <pedropardo021@gmail.com>
2024-04-17 21:06:57 -0500

    edited for usage

--- decision_tree.py
+++ decision_tree.py
@@ -1,14 +1,19 @@
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-heart_failure_clinical_records = fetch_ucirepo(id=519) 
-  
-# data (as pandas dataframes) 
-X = heart_failure_clinical_records.data.features 
-y = heart_failure_clinical_records.data.targets 
-  
-# metadata 
-print(heart_failure_clinical_records.metadata) 
-  
-# variable information 
-print(heart_failure_clinical_records.variables) 
+import ssl
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
+
+# Set the default SSL context to not verify certificates
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Fetch the dataset
+heart_failure_clinical_records = fetch_ucirepo(id=519)
+
+# Separate features and target variable
+X = heart_failure_clinical_records.data.features
+y = heart_failure_clinical_records.data.targets
+
+# Metadata
+print(heart_failure_clinical_records.metadata)
+
+# Variable information
+print(heart_failure_clinical_records.variables)
